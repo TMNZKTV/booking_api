1053da286b24c74f6dfff433cc24eabae72ff1b9
TUMEN ZORIKTUEV <69411964+TMNZKTV@users.noreply.github.com>
2022-03-15 17:57:33 +0800

    Причина удаления, стили, проверка телефона, столы в логах

--- database/migrations/2022_03_14_082507_add_reason_failed_to_logs_table.php
+++ database/migrations/2022_03_14_082507_add_reason_failed_to_logs_table.php
@@ -26,7 +26,7 @@ class AddReasonFailedToLogsTable extends Migration
     public function down()
     {
         Schema::table('logs', function (Blueprint $table) {
-            $table->string('reason_failed')->nullable();
+            $table->string('reason_failed');
         });
     }
 }
